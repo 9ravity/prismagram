9e5e1558648f8b40b4efa4fd732baddf7c350de1
9ravity <shegoback@naver.com>
2020-04-21 21:11:05 +0900

    update

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -4,12 +4,7 @@ export default {
   Query: {
     me: async (_, __, { request }) => {
       const { user } = request;
-      const userProfile = await prisma.user({ id: user.id });
-      const posts = await prisma.user({ id: user.id }).posts;
-      return {
-        user: userProfile,
-        posts
-      };
-    }
-  }
+      return await prisma.user({ id: user.id });
+    },
+  },
 };
